3176d4b29b4070644caf5fdfa63208aecc5c22fa
TREVOR CHILDS <trevorchilds8@gmail.com>
2022-11-07 13:24:11 -0700

    finished video player

--- 11 - custom html5 video player/script.js
+++ 11 - custom html5 video player/script.js
@@ -10,9 +10,40 @@ function togglePlay() {
   const method = video.paused ? "play" : "pause";
   video[method]();
 
+  // removed the obsolete update button function
   if (video.paused) toggle.innerHTML = "❚ ❚";
   else toggle.innerHTML = "►";
 }
+function handleRangeUpdate() {
+  // square brackets to pass in the name of a child function
+  video[this.name] = this.value;
+}
+function handleProgress() {
+  let percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+skipButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    video.currentTime += parseFloat(button.dataset.skip);
+  })
+);
+// event listeners, do not need the play and pause
 
 video.addEventListener("click", togglePlay);
 toggle.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", handleProgress);
+
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
