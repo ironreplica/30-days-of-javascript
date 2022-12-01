5df9ef4f3c8d085cb45bc6d42395249edea045da
TREVOR CHILDS <trevorchilds8@gmail.com>
2022-12-01 13:20:30 -0700

    finished camera

--- 19 - webcam fun/script.js
+++ 19 - webcam fun/script.js
@@ -9,7 +9,102 @@ function getVideo() {
     .getUserMedia({ video: true, audio: false })
     .then((localMediaStream) => {
       console.log(localMediaStream);
+
+      //  DEPRECIATION :
+      //       The following has been depreceated by major browsers as of Chrome and Firefox.
+      //       video.src = window.URL.createObjectURL(localMediaStream);
+      //       Please refer to these:
+      //       Deprecated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+      //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
+
+      video.srcObject = localMediaStream;
+      video.play();
+      paintToCanvas();
+    })
+    .catch((err) => {
+      console.error(`OH NO!!!`, err);
     });
 }
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
 
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, height);
+    //ctx.globalAlpha = 0.01;
+    // pixels = redEffect(pixels);
+    //pixels = rgbSplit(pixels);
+    pixels = greenScreen(pixels);
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src="${data}" alt = "me"/>`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    // the pixels are automatically fit into a large array storing every pixel and its rgba values in order
+    //increment through the loop by 4 so that way it starts from the R in RGBA every cycle.
+    pixels.data[i + 0] = pixels.data[i] + 200; //r
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //g
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //b
+  }
+  return pixels;
+}
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    // the pixels are automatically fit into a large array storing every pixel and its rgba values in order
+    //increment through the loop by 4 so that way it starts from the R in RGBA every cycle.
+    pixels.data[i - 150] = pixels.data[i + 0]; //r
+    pixels.data[i + 10] = pixels.data[i + 1]; //g
+    pixels.data[i - 150] = pixels.data[i + 2]; //b
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach((input) => {
+    levels[input.name] = input.value;
+  });
+  console.log(levels);
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
 getVideo();
+
+//canvas.addEventListener("load", paintToCanvas);
+video.addEventListener("canplay", paintToCanvas);
+//paintToCanvas();
